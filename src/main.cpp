dbc9823c92171e11f701b1291934cb52f14fc6e1
l3w1sh3r <nos4bugs@gmail.com>
2025-10-02 14:19:25 +0700

    update include path

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <SDL2/SDL.h>
+#include <SDL.h>
 #include <iostream>
 #include "graphics.h"
 #include "logic.h"
@@ -6,55 +6,34 @@
 
 int main(int argc, char *argv[])
 {
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    Graphics graphics;
+    if (!graphics.init("FruitBox", SCREEN_HEIGHT, SCREEN_WIDTH))
     {
-        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    // Create a window
-    SDL_Window *window = SDL_CreateWindow(
-        "Fruit Box",            // Window title
-        SDL_WINDOWPOS_CENTERED, // x position
-        SDL_WINDOWPOS_CENTERED, // y position
-        800,                    // width
-        600,                    // height
-        SDL_WINDOW_SHOWN        // flags
-    );
-
-    if (window == nullptr)
-    {
-        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
-
-    // Main loop flag
+    Logic logic;
     bool quit = false;
     SDL_Event e;
 
-    // Main loop
     while (!quit)
     {
-        // Handle events on queue
-        while (SDL_PollEvent(&e) != 0)
+        while (SDL_PollEvent(&e))
         {
-            // User requests quit
             if (e.type == SDL_QUIT)
             {
                 quit = true;
             }
+            logic.handleEvent(e);
         }
 
-        // TODO: Here you’ll draw things in the future
+        logic.update();
+
+        graphics.clear();
+        graphics.drawBoard();
+        graphics.present();
     }
 
-    // Destroy window
-    SDL_DestroyWindow(window);
-
-    // Quit SDL subsystems
-    SDL_Quit();
-
+    graphics.shutdown();
     return 0;
 }
